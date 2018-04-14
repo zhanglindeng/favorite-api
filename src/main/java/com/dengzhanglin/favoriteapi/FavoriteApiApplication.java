a97cbfc5541bbf6501f6adbef19a9a2e01127d5d
zhanglindeng <zhanglindeng@outlook.com>
2018-04-15 01:23:49 +0800

    init

---
+++ src/main/java/com/dengzhanglin/favoriteapi/FavoriteApiApplication.java
@@ -0,0 +1,12 @@
+package com.dengzhanglin.favoriteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoriteApiApplication.class, args);
+    }
+}
