95a9899d989bd90b76db5f031fe6a8c84431e5fc
zhanglindeng <zhanglindeng@outlook.com>
2018-04-18 21:48:28 +0800

    @Email validation

--- src/main/java/com/dengzhanglin/favoriteapi/payload/LoginRequest.java
+++ src/main/java/com/dengzhanglin/favoriteapi/payload/LoginRequest.java
@@ -1,10 +1,12 @@
 package com.dengzhanglin.favoriteapi.payload;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class LoginRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
--- src/main/java/com/dengzhanglin/favoriteapi/payload/RegisterRequest.java
+++ src/main/java/com/dengzhanglin/favoriteapi/payload/RegisterRequest.java
@@ -1,11 +1,13 @@
 package com.dengzhanglin.favoriteapi.payload;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class RegisterRequest {
     @NotBlank
     @Size(min = 3, max = 50)
+    @Email
     private String email;
 
     @NotBlank
