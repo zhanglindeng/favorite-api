c6f75c7fd6cd177256faf9bf6760a5eb160f1c31
zhanglindeng <zhanglindeng@outlook.com>
2018-04-21 17:36:29 +0800

    website del

--- src/main/java/com/dengzhanglin/favoriteapi/controller/WebsiteController.java
+++ src/main/java/com/dengzhanglin/favoriteapi/controller/WebsiteController.java
@@ -12,13 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.Instant;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/website")
@@ -30,6 +28,40 @@ public class WebsiteController {
     @Autowired
     public WebsiteController(WebsiteRepository websiteRepository) {
         this.websiteRepository = websiteRepository;
+    }
+
+    @PostMapping(value = "/del/{id}")
+    public ResponseEntity<?> del(@PathVariable("id") Long id, @CurrentUser UserPrincipal userPrincipal) {
+        String loggerInfo = "[website:del]";
+
+        // user_id
+        Long userId = userPrincipal.getId();
+        this.logger.info(loggerInfo + " " + userId + " " + id);
+
+        Optional<Website> row = this.websiteRepository.findById(id);
+        if (!row.isPresent()) {
+            this.logger.info(loggerInfo + " website");
+            return ResponseEntity.ok(new ApiResponse(false, "网站没有找到"));
+        }
+        Website website = row.get();
+        if (website.getUser() == null) {
+            this.logger.info(loggerInfo + " user");
+            return ResponseEntity.ok(new ApiResponse(false, "网站没有找到"));
+        }
+        if (website.getCategory() == null) {
+            this.logger.info(loggerInfo + " category");
+            return ResponseEntity.ok(new ApiResponse(false, "网站没有找到"));
+        }
+        if (!website.getUser().getId().equals(userId)) {
+            this.logger.info(loggerInfo + " user_id");
+            return ResponseEntity.ok(new ApiResponse(false, "网站没有找到"));
+        }
+
+        // TODO 怎么知道是否删除成功
+        this.websiteRepository.delete(website);
+        this.logger.info(loggerInfo + " deleted " + userId + " " + id);
+
+        return ResponseEntity.ok(new ApiResponse(true, "OK"));
     }
 
     @PostMapping(value = "/add")
