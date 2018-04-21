099a38a7060c1b40973a1c8341dbf3f7685615d9
zhanglindeng <zhanglindeng@outlook.com>
2018-04-21 19:14:32 +0800

    sort

--- src/main/java/com/dengzhanglin/favoriteapi/controller/CategoryController.java
+++ src/main/java/com/dengzhanglin/favoriteapi/controller/CategoryController.java
@@ -14,10 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping(value = "/category")
@@ -70,14 +69,57 @@ public class CategoryController {
         // user_id
         Long userId = userPrincipal.getId();
 
+        // sort
+        Integer sort = 0;
+
         List<Category> categories = this.categoryRepository.findByUserOrderBySort(userId);
 
         Iterator<Category> iterator = categories.iterator();
 
         Category temp;
+        Map<Long, Integer> sorts = new HashMap<>();
         while (iterator.hasNext()) {
             temp = iterator.next();
             logger.info(loggerInfo + temp.getName());
+            sorts.put(temp.getId(), temp.getSort());
+        }
+
+        Iterator iterator1 = sorts.entrySet().iterator();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String updatedAt = simpleDateFormat.format(new Date());
+        // 放在第一个
+        if (addCategoryRequest.getFirst()) {
+            while (iterator1.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator1.next();
+                Long tempId = (Long) entry.getKey();
+                Integer tempSort = (Integer) entry.getValue();
+                this.logger.info(loggerInfo + " " + tempId + " " + (this.categoryRepository.modifyById(tempSort + 1, tempId, updatedAt)));
+            }
+        } else {
+            // after
+            Long after = addCategoryRequest.getAfter();
+            while (iterator1.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator1.next();
+                Long tempId = (Long) entry.getKey();
+                Integer tempSort = (Integer) entry.getValue();
+                this.logger.info(loggerInfo + " " + tempId + " " + tempSort);
+                if (tempId.equals(after)) {
+                    // new category sort
+                    sort = tempSort + 1;
+                    break;
+                }
+            }
+            Iterator iterator2 = sorts.entrySet().iterator();
+            while (iterator2.hasNext()) {
+                Map.Entry entry = (Map.Entry) iterator2.next();
+                Long tempId = (Long) entry.getKey();
+                Integer tempSort = (Integer) entry.getValue();
+                this.logger.info(loggerInfo + " " + tempId + " " + tempSort);
+                if (tempSort >= sort) {
+                    // 后面的加 sort + 1
+                    this.logger.info(loggerInfo + " " + tempId + " " + (this.categoryRepository.modifyById(tempSort + 1, tempId, updatedAt)));
+                }
+            }
         }
 
         // user
@@ -88,7 +130,7 @@ public class CategoryController {
         Category category = new Category();
         category.setName(name);
         category.setUser(user);
-        // TODO sort
+        category.setSort(sort);
         Instant instant = Instant.now();
         category.setCreatedAt(instant);
         category.setUpdatedAt(instant);
